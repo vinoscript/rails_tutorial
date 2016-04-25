5346faadc28c10e42ce3f6d1c6dabe3c1686ba3d
vinoscript <wstikk@gmail.com>
2016-04-25 01:00:46 +0000

    step 8 done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,25 @@ def show
   @post = Post.find(params[:id])
 end
 
+def new
+  @post = Post.new
+end
+
+def create
+  @post = Post.new(post_params)
+
+  if @post.save
+    redirect_to posts_path
+  else
+    render :new
+  end
+end
+
+protected
+
+def post_params
+  params.require(:post).permit(:title, :author, :text)
+end
 
 
 end
